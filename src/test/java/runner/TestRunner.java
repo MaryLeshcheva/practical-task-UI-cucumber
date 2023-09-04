a90cb0acebf04f9e0992f493700df97c9229a6e6
Mariya Leshcheva <maleshch@icloud.com>
2023-09-04 13:38:22 +0300

    Adding the Hooks.java file. Editing TestRunner.java, StepForUITest.java, CheckAddProductTest.feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,9 +10,9 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"), //@DataBase, @UI
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"), //@DataBase, @UI, @all
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps, hooks"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
 public class TestRunner {
